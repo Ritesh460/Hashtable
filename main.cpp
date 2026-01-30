714f4a65989201831006f3a2683131ffbca5e6d6
Neptune990 <460219@bsd48.org>
2026-01-30 09:34:18 -0800

    Basic Steps

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <random>
+
+int main () {
+  return 0;
+}
+
+/*
+Steps: 
+1. get functions from student program and configure them to fit with hash table
+2. hash table starts with at least 100 slots and deal with collisions, and chaining(linked lists)
+3. Have code for cases such as 3 or more collisions, where table is doubled in slots and students are rehashed
+4. Create random student generator which student list program takes and adds basic componennets like name, ID, and GPA
+*/
